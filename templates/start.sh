e9e0ee00905c34e4382409a779c99a37ff61cc07
vchaudhary-eng <vchaudhary@markscan.co.in>
2025-07-17 16:45:31 +0530

    Delete templates/start.sh

--- templates/start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Install Playwright browsers if not already installed
-python -m playwright install
-
-# Start FastAPI app
-gunicorn main:app --bind 0.0.0.0:$PORT --worker-class uvicorn.workers.UvicornWorker
