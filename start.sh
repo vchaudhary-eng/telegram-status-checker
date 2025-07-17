5180270ceffe40841701b40fb03d97123c571b4a
vchaudhary-eng <vchaudhary@markscan.co.in>
2025-07-17 18:11:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Set the path for Playwright to install its browsers
+export PLAYWRIGHT_BROWSERS_PATH=/mnt/cache/.playwright
+
 echo "Installing Playwright browsers..."
 python -m playwright install chromium --with-deps
 
